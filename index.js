67ce814977e4cf809b6f94ec67349b7ef8a56933
Gastón Quiroga <gasquiroga1@gmail.com>
2022-11-29 20:56:36 -0300

    clase 3

---
+++ index.js
@@ -0,0 +1,87 @@
+// 1)
+
+// let i = 1000
+
+// for ( let i = 0; i <= 9; i = i + 1 ) {
+//     console.log( "¡¡Oh no, entré en un bucleeee!!" )
+
+// }
+
+
+
+
+// 2)
+
+// let i = 1000
+
+// for ( let i = 20; i < 70; i = i + 1 ) {
+//      console.log( i )
+
+// }
+
+
+
+
+// 3)
+
+// const nombreDelEstudiante = "Gastón"
+
+// for ( let i = 0; i < nombreDelEstudiante.length; i++ ) { 
+//     console.log( nombreDelEstudiante [i] )
+
+// }
+
+
+
+
+
+// 4)
+
+
+//  let acc = 0
+
+
+//  for ( let i = 10; i <= 100; i++ ) {
+
+//      acc++
+
+//  }
+
+//  console.log( acc )
+
+
+
+
+// 5)
+
+// let acc = 0
+
+// for ( let i = 0; i < 10; i++ ){
+
+//     if ( i % 2 == 1 ) {
+//         acc++
+
+//     }
+
+// }
+
+// console.log (acc)
+
+// 6)
+
+
+//  const palabra = "otorrinolaringologo"
+
+//  let acc = 0
+
+//  for (  let i = 0; i < palabra.length; i++ ) {
+
+//      if ( palabra[i].match(/[aeiouAEIOUáéíóúÁÉÍÓÚ]/)) {
+
+//           acc++
+        
+//      }
+
+//  }
+
+//  console.log ( `La cantidad de vocales es de ${acc}` )
